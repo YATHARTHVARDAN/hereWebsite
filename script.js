46e09a2918d968f43aca9c818ff1cf7d36c7bc9e
YATHARTH VARDAN <yatharthvardan@gmail.com>
2020-10-08 23:56:15 +0530

    done

--- script.js
+++ script.js
@@ -11,6 +11,8 @@ window.onload = async ()=>{
     dataLoader.style.display = "none";
 }
 
+console.log(dataTodisplay['North East Delhi']);
+
 const apiKey = 'Yd-fnbk9FQ9yAsp35VV5rXMlCnMVJTS4eBk2f3wIkns';
 var n = 0;
 
